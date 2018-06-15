7816d9710dc4d5ddf27f81277d149eeee1278f7c
rocky <rock.song@hotmail.com>
2018-06-15 14:30:07 +0800

    update more (test branch merging)

--- ports/nxp_rt105/omv/py/py_image.c
+++ ports/nxp_rt105/omv/py/py_image.c
@@ -2631,7 +2631,7 @@ static void py_blob_print(const mp_print_t *print, mp_obj_t self_in, mp_print_ki
 {
     py_blob_obj_t *self = self_in;
     mp_printf(print,
-              "{x:%d, y:%d, w:%d, h:%d, pixels:%d, cx:%d, cy:%d, rotation:%f, code:%d, count:%d}",
+              "{\"x\":%d, \"y\":%d, \"w\":%d, \"h\":%d, \"pixels\":%d, \"cx\":%d, \"cy\":%d, \"rotation\":%f, \"code\":%d, \"count\":%d}",
               mp_obj_get_int(self->x),
               mp_obj_get_int(self->y),
               mp_obj_get_int(self->w),
