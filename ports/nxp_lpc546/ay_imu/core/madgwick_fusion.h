dc0800482a9f23023dca92bdb2a0a3fe417cad28
rocky <rock.song@hotmail.com>
2019-03-11 19:18:48 +0800

    remove most of non relevant files for mfn demo

--- ports/nxp_lpc546/ay_imu/core/madgwick_fusion.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MADGWICK_FUSION_H__
-#define __MADGWICK_FUSION_H__
-
-#include "vector.h"
-
-void madgwick_ahrs_update(q_t *q, v3_t g, v3_t a, v3_t m, float deltaT);
-
-#endif
--- ports/nxp_lpc546/boards/lpx54608/mpconfigboard.mk
+++
@@ -1,6 +0,0 @@
-MCU_SERIES = LPC54
-# must be the same name defined in fsl_device_registers.h
-CMSIS_MCU = LPC54608
-SDK_MCU = CPU_LPC54608J512BD208
-AF_FILE = boards/lpc54608_af.csv
-LD_FILE = boards/lpc54608.ld
