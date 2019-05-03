6247a4f32efcdd2e1e8547ac8db3bf2c629bd914
rocky <rock.song@hotmail.com>
2019-05-03 21:05:28 +0800

    Restored gnerated files for evk board

---
+++ ports/nxp_rt1050_60/build-imxrtevk105x/genhdr/pins.h
@@ -0,0 +1,77 @@
+extern const pin_obj_t pin_AD_B0_01;
+extern const pin_af_obj_t pin_AD_B0_01_af[];
+extern const pin_obj_t pin_AD_B0_02;
+extern const pin_af_obj_t pin_AD_B0_02_af[];
+extern const pin_obj_t pin_AD_B0_03;
+extern const pin_af_obj_t pin_AD_B0_03_af[];
+extern const pin_obj_t pin_AD_B0_09;
+extern const pin_af_obj_t pin_AD_B0_09_af[];
+extern const pin_obj_t pin_AD_B0_10;
+extern const pin_af_obj_t pin_AD_B0_10_af[];
+extern const pin_obj_t pin_AD_B0_11;
+extern const pin_af_obj_t pin_AD_B0_11_af[];
+extern const pin_obj_t pin_AD_B0_12;
+extern const pin_af_obj_t pin_AD_B0_12_af[];
+extern const pin_obj_t pin_AD_B0_13;
+extern const pin_af_obj_t pin_AD_B0_13_af[];
+extern const pin_obj_t pin_AD_B1_00;
+extern const pin_af_obj_t pin_AD_B1_00_af[];
+extern const pin_obj_t pin_AD_B1_01;
+extern const pin_af_obj_t pin_AD_B1_01_af[];
+extern const pin_obj_t pin_AD_B1_02;
+extern const pin_af_obj_t pin_AD_B1_02_af[];
+extern const pin_obj_t pin_AD_B1_03;
+extern const pin_af_obj_t pin_AD_B1_03_af[];
+extern const pin_obj_t pin_AD_B1_04;
+extern const pin_af_obj_t pin_AD_B1_04_af[];
+extern const pin_obj_t pin_AD_B1_05;
+extern const pin_af_obj_t pin_AD_B1_05_af[];
+extern const pin_obj_t pin_AD_B1_06;
+extern const pin_af_obj_t pin_AD_B1_06_af[];
+extern const pin_obj_t pin_AD_B1_07;
+extern const pin_af_obj_t pin_AD_B1_07_af[];
+extern const pin_obj_t pin_AD_B1_08;
+extern const pin_af_obj_t pin_AD_B1_08_af[];
+extern const pin_obj_t pin_AD_B1_10;
+extern const pin_af_obj_t pin_AD_B1_10_af[];
+extern const pin_obj_t pin_AD_B1_11;
+extern const pin_af_obj_t pin_AD_B1_11_af[];
+extern const pin_obj_t pin_SD_B0_00;
+extern const pin_af_obj_t pin_SD_B0_00_af[];
+extern const pin_obj_t pin_SD_B0_01;
+extern const pin_af_obj_t pin_SD_B0_01_af[];
+extern const pin_obj_t pin_SD_B0_02;
+extern const pin_af_obj_t pin_SD_B0_02_af[];
+extern const pin_obj_t pin_SD_B0_03;
+extern const pin_af_obj_t pin_SD_B0_03_af[];
+extern const pin_obj_t pin_WAKEUP;
+extern const pin_af_obj_t pin_WAKEUP_af[];
+extern const pin_obj_t * const pin_adc1[];
+extern const pin_obj_t * const pin_adc2[];
+extern const pin_obj_t * const pin_adc3[];
+#define pyb_pin_D0_RX pin_AD_B1_07
+#define pyb_pin_D1_TX pin_AD_B1_06
+#define pyb_pin_D2_INT0 pin_AD_B0_11
+#define pyb_pin_D3_INT1 pin_AD_B1_08
+#define pyb_pin_D4 pin_AD_B0_09
+#define pyb_pin_D5_PWM1 pin_AD_B0_10
+#define pyb_pin_D6_PWM2 pin_AD_B1_02
+#define pyb_pin_D7_PWM3 pin_AD_B1_03
+#define pyb_pin_D8 pin_AD_B0_03
+#define pyb_pin_D9_PWM4 pin_AD_B0_02
+#define pyb_pin_D10_SS pin_SD_B0_01
+#define pyb_pin_D11_MO pin_SD_B0_02
+#define pyb_pin_D12_MI pin_SD_B0_03
+#define pyb_pin_D13_CK pin_SD_B0_00
+#define pyb_pin_D14_SDA pin_AD_B0_01
+#define pyb_pin_D15_SCL pin_AD_B0_02
+#define pyb_pin_A0 pin_AD_B1_10
+#define pyb_pin_A1 pin_AD_B1_11
+#define pyb_pin_A2 pin_AD_B1_04
+#define pyb_pin_A3 pin_AD_B1_05
+#define pyb_pin_A4 pin_AD_B1_01
+#define pyb_pin_A5 pin_AD_B1_00
+#define pyb_pin_LED pin_AD_B0_09
+#define pyb_pin_KEY pin_WAKEUP
+#define pyb_pin_DBG_RXD pin_AD_B0_13
+#define pyb_pin_DBG_TXD pin_AD_B0_12
