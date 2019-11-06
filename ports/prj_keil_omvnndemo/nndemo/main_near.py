5869fb1d9a4c0eb1005ac0314fdd76f7feb4eb39
RockySong <rock.song@hotmail.com>
2019-11-06 17:29:43 +0800

    Add main_near.py for 50cm-70cm optimal distances Add main_far.py for 100cm-140cm optimal distances.

---
+++ ports/prj_keil_omvnndemo/nndemo/main_near.py
@@ -0,0 +1,23 @@
+import sensor, time, image, nndemo
+sensor.reset()
+# Sensor settings
+sensor.set_auto_gain(True)
+sensor.set_auto_exposure(True)
+sensor.set_auto_whitebal(True)
+sensor.set_brightness(2)
+sensor.set_contrast(1)
+sensor.set_gainceiling(16)
+# HQVGA and GRAYSCALE are the best for face tracking.
+sensor.set_framesize(sensor.VGA)
+sensor.set_windowing((256,192))
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framerate(0<<9 | 1<<11)
+sensor.skip_frames(time = 700)          # Wait for settings take effect.
+sensor.set_auto_gain(False)
+# FPS clock
+clock = time.clock()
+nndemo.show(0, index=0, data=0)
+while (True):
+    clock.tick()
+    nndemo.show(0, index=0, data = 1)
+    print(clock.fps())
