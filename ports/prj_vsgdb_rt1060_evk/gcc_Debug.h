aefe7e678e2413e6b4ea484302c41f673ebbf2ed
Rocky Song <rock.song@hotmail.com>
2020-04-12 21:59:45 +0800

    preliminary support ozone and visualgdb project

---
+++ ports/prj_vsgdb_rt1060_evk/gcc_Debug.h
@@ -0,0 +1,106 @@
+/*
+	This file is only used by IntelliSense (VisualStudio code suggestion system)
+	DO NOT INCLUDE THIS FILE FROM YOUR ACTUAL SOURCE FILES.
+	This file lists the preprocessor macros extracted from your GCC.
+	It is needed for IntelliSense to parse other header files correctly.
+*/
+#if defined(_MSC_VER) || defined (__SYSPROGS_CODESENSE__)
+#pragma clang diagnostic push
+
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+#ifndef __FPU_PRESENT
+#define __FPU_PRESENT 1
+#endif
+#ifndef USB_DEVICE_CONFIG_BUFFER_PROPERTY_CACHEABLE
+#define USB_DEVICE_CONFIG_BUFFER_PROPERTY_CACHEABLE 1
+#endif
+#ifndef ARM_MATH_CM7
+#define ARM_MATH_CM7 
+#endif
+#ifndef N_THUMB
+#define N_THUMB 
+#endif
+#ifndef NDEBUG
+#define NDEBUG 
+#endif
+#ifndef CPU_MIMXRT1052DVL6A
+#define CPU_MIMXRT1052DVL6A 
+#endif
+#ifndef MCU_SERIES_RT105
+#define MCU_SERIES_RT105 
+#endif
+#ifndef MCU_SERIES_RT106
+#define MCU_SERIES_RT106 
+#endif
+#ifndef USB_STACK_BM
+#define USB_STACK_BM 
+#endif
+#ifndef __BYTE_ORDER__
+#define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
+#endif
+#ifndef MICROPY_QSTR_EXTRA_POOL
+#define MICROPY_QSTR_EXTRA_POOL mp_qstr_frozen_const_pool
+#endif
+#ifndef MICROPY_MODULE_FROZEN_MPY
+#define MICROPY_MODULE_FROZEN_MPY 
+#endif
+#ifndef FFCONF_H
+#define FFCONF_H '\"oofatfs/ffconf.h\"'
+#endif
+#ifndef HARD_FP
+#define HARD_FP 
+#endif
+#ifndef FSL_SDK_ENABLE_DRIVER_CACHE_CONTROL
+#define FSL_SDK_ENABLE_DRIVER_CACHE_CONTROL 1
+#endif
+#ifndef XIP_EXTERNAL_FLASH
+#define XIP_EXTERNAL_FLASH 
+#endif
+#ifndef M_PI
+#define M_PI 3.141592654f
+#endif
+#ifndef M_PI_2
+#define M_PI_2 1.570796327f
+#endif
+#endif
+
+// --- Include directories begin --- //
+//..\..
+//..\..\py
+//..\..\extmod
+//..\..\lib
+//..\..\lib\cmsis\inc
+//..\nxp_rt1050_60
+//..\nxp_rt1050_60\boards\imxrtevk105x
+//..\nxp_rt1050_60\mcusdk
+//..\nxp_rt1050_60\mcusdk\drivers
+//..\nxp_rt1050_60\mcusdk\utilities
+//..\nxp_rt1050_60\cmsis
+//..\nxp_rt1050_60\build-imxrtevk105x
+//..\nxp_rt1050_60\usb_cdc_msc
+//..\nxp_rt1050_60\mcusdk\middleware\sdkusb\include
+//..\nxp_rt1050_60\mcusdk\middleware\sdkusb\osa
+//..\nxp_rt1050_60\mcusdk\middleware\sdkusb\device
+//..\nxp_rt1050_60\mcusdk\middleware\sdkusb\phy
+//..\nxp_rt1050_60\sdmmc
+//..\nxp_rt1050_60\omv
+//..\nxp_rt1050_60\omv\py
+//..\nxp_rt1050_60\omv\img
+//..\nxp_rt1050_60\omv\nn
+//..\..\lib\oofatfs
+//..\..\lib\mp-readline
+//..\..\extmod\rt_thread
+//..\..\extmod\rt_thread\include\libc
+//..\..\extmod\rt_thread\include
+//..\..\drivers\m8266\lib_new
+//..\..\drivers\m8266
+//..\nxp_rt1050_60\mcusdk\utilities\io
+//..\nxp_rt1050_60\mcusdk\utilities\log
+//..\nxp_rt1050_60\mcusdk\utilities\str
+//..\nxp_rt1050_60\flegftl
+// --- Include directories end --- //
+
+#pragma clang diagnostic pop
